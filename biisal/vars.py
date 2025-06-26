88696807a825dc534dac0a17ea2ef8e4cf15455f
Ace300331 <kujoora923@gmail.com>
2025-06-26 10:09:12 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -18,8 +18,8 @@ class Var(object):
     name = str(getenv('name', 'Ace_file2link_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002654701895'))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002795692756'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002786845877'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002786845877'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
